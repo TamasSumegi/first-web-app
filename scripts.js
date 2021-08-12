a6d30303f84e1df75a4cb6c6ce8abd744e259fd7
TamasSumegi <sumegit@gmail.com>
2021-08-12 04:17:28 +0200

    ..

--- scripts.js
+++ scripts.js
@@ -1,1 +1,8 @@
 /* javascript for first-web-app*/
+let data = {
+    photo: 'images/raspberries-3454504_640.jpg',
+    title: 'Málna',
+    description: 'Szép, érett málnaszemek'
+  };
+
+  $('#photo').attr('src', data.photo);
