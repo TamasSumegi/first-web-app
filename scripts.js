a51c2ea2603688c30e710e64ee503fd3324c9a20
TamasSumegi <sumegit@gmail.com>
2021-08-14 01:45:04 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,81 +1,21 @@
 /* javascript for first-web-app*/
-let data0 = {
-  photo: 'images/raspberries-3454504_640.jpg',
-  title: 'Málna',
-  description: 'Szép, érett málnaszemek'
-};
+const imagesdata =[ {   photo: "images/raspberries-3454504_640.jpg",   title: "Málna",   description: "Szép, érett málnaszemek"},
+{photo: "cows-2641195_640.jpg", title: "Tehenek", description: "Tehenek az alpesi legelőn"},
+{photo: "animal-3729821_640.jpg", title: "Őz", description: "Kíváncsi őzike"},
+{photo: "sunflowers-3792914_640.jpg", title: "Napraforgók", description: "Napraforgótenger"},
+{photo: "roses-3189198_640.jpg", title: "Rózsák", description: "Rózsavirágzatok"},
+{photo: "swan-2270980_640.jpg", title: "Hatyú", description: "Hattyú a vízen"},
+{photo: "parrot-3695678_640.jpg", title: "Papagáj", description: "Nagy kék arapapagáj"},
+{photo: "owl-50267_640.jpg", title: "Bagoly", description: "Erdei fülesbagoly portréja szemből"},
+{photo: "lion-3260558_640.jpg", title: "Oroszlán", description: "Nagycica"},
+{photo: "honey-bee-6506809_640.jpg", title: "Méh", description: "Mézelő méh begyűjtés közben"},
+{photo: "elephant-3832516_640.jpg", title: "Elefánt", description: "Magányos elefánt"},
+{photo: "tasmanian-devil-6479685_640.jpg", title: "Tasmán ördög", description: "Tasmán ördög szemből"},
+{photo: "bald-eagle-6481346_640.jpg", title: "Sas", description: "Fehér fejkű halászsas (rétisas)"}
+];
 
-const data1 = {
-  photo: 'cows-2641195_640.jpg',
-  title: 'Tehenek',
-  description: 'Tehenek az alpesi legelőn'
-};
+var currentPhoto = 0;
+$('#photo').attr('src', imagesData[currentPhoto].photo);
+$("#photoTitle").text(imagesData[currentPhoto].title);
+$("#phototext").text(imagesData[currentPhoto].description);
 
-const data2 = {
-  photo: 'animal-3729821_640.jpg',
-  title: 'Őz',
-  description: 'Kíváncsi őzike'
-};
-
-const data3 = {
-    photo: 'sunflowers-3792914_640.jpg',
-    title: 'Napraforgók',
-    description: 'Napraforgótenger'
-  };
-
-const data4 = {
-  photo: 'roses-3189198_640.jpg',
-  title: 'Rózsák',
-  description: 'Rózsavirágzatok'
-};
-const data5 = {
-    photo: 'swan-2270980_640.jpg',
-    title: 'Hatyú',
-    description: 'Hattyú a vízen'
-};
-
-const data6 = {
-    photo: 'parrot-3695678_640.jpg',
-    title: 'Papagáj',
-    description: 'Nagy kék arapapagáj'
-};
-
-const data7 = {
-    photo: 'owl-50267_640.jpg',
-    title: 'Bagoly',
-    description: 'Erdei fülesbagoly portréja szemből'
-};
-
-const data8 = {
-    photo: 'lion-3260558_640.jpg',
-    title: 'Oroszlán',
-    description: 'Nagycica'
-};
-
-const data9 = {
-    photo: 'honey-bee-6506809_640.jpg',
-    title: 'Méh',
-    description: 'Mézelő méh begyűjtés közben'
-};
-
-const data10 = {
-    photo: 'elephant-3832516_640.jpg',
-    title: 'Elefánt',
-    description: 'Magányos elefánt'
-};
-
-const data11 = {
-    photo: 'tasmanian-devil-6479685_640.jpg',
-    title: 'Tasmán ördög',
-    description: 'Tasmán ördög szemből'
-};
-
-const data12 = {
-    photo: 'bald-eagle-6481346_640.jpg',
-    title: 'Sas',
-    description: 'Fehér fejkű halászsas (rétisas)'
-};
-
-$('#photo').attr('src', data0.photo);
-$("#photoTitle").text(data0.title);
-$("#phototext").text(data0.description);
