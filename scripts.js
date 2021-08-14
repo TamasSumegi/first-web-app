5da840400334314b9f016033fb58f3cc65430f07
TamasSumegi <sumegit@gmail.com>
2021-08-14 02:45:35 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -15,7 +15,7 @@ const imagesdata = [
 {photo:"bald-eagle-6481346_640.jpg", title:"Sas", description:"Fehér fejkű halászsas (rétisas)"}
 ];
 
-let currentPhoto = 0;
+let currentPhoto = 1;
 $('#photo').attr('src', imagesdata[currentPhoto].photo);
 $("#photoTitle").text(imagesdata[currentPhoto].title);
 $("#phototext").text(imagesdata[currentPhoto].description);
